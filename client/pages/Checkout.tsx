c53f270e82f0b9a5b2b6a909f3c66c505c0732b4
VASANT <vasantjv2005@gmail.com>
2025-07-18 19:19:17 +0530

    completed project of course central electro-zone

---
+++ client/pages/Checkout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { PlaceholderPage } from "@/components/PlaceholderPage";
+
+const Checkout: React.FC = () => {
+  return (
+    <PlaceholderPage
+      title="Checkout"
+      description="Secure checkout process to complete your order with multiple payment options."
+    />
+  );
+};
+
+export default Checkout;
